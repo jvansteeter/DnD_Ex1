60a76e98f5127eaa9ce74b5ceeede5332d66e68f
Joshua Van Steeter <spam.magnet1991@gmail.com>
2016-06-02 16:28:06 -0600

    m

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var passport = require('passport');
 
-var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
