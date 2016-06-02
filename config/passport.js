56018e0513a3c9289be9cf7cef5696e1d914e456
Josh's AWS Amazon Server <spam.magnet1991@gmail.com>
2016-06-02 01:19:17 +0000

    first commit

---
+++ config/passport.js
@@ -0,0 +1,26 @@
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+
+passport.use(new LocalStrategy(function (username, password, done)
+{
+	console.log("in passport.js");
+	User.findOne({ username: username }, function (error, user)
+	{
+		if (error)
+		{
+			return done(error);
+		}
+		if(!user)
+		{
+			return done(null, false, { message: 'Incorrect username.' });
+		}
+		if(!user.checkPassword(password))
+		{
+			return done(null, false, { message: 'Incorrectpassword.' });
+		}
+		console.log("returning user");
+		return done(null, user);
+	});
+}));
