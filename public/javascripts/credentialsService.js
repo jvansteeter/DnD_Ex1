54e55e27540a651ab76d6d53620a13dd83d39aab
Joshua Van Steeter <spam.magnet1991@gmail.com>
2016-06-02 13:26:15 -0600

    e

---
+++ public/javascripts/credentialsService.js
@@ -0,0 +1,39 @@
+var clientApp = angular.module('clientApp');
+
+clientApp.service('Credentials', function($window)
+{
+    var credService = {};
+
+    credService.getUsername = function()
+    {
+        //return username;
+        var myData = $window.sessionStorage.getItem("user");
+        return myData;
+    };
+    credService.getPassword = function()
+    {
+        //return password;
+        var myData = $window.sessionStorage.getItem("password");
+        return myData;
+    };
+    /*credService.getTodoList = function()
+    {
+        var data = $window.sessionStorage.getItem("todoList");
+        return JSON.parse(data);
+    };*/
+    credService.setUsername = function(name)
+    {
+        //username = name;
+        $window.sessionStorage.setItem("user", name);
+    };
+    credService.setPassword = function(secret)
+    {
+        //password = secret;
+        $window.sessionStorage.setItem("password", secret);
+    };
+    /*credService.setTodoList = function(todoList)
+    {
+        $window.sessionStorage.setItem("todoList", JSON.stringify(todoList));
+    }*/
+    return credService;
+});
---
+++ public/javascripts/loginController.js
@@ -0,0 +1,81 @@
+var clientApp = angular.module('clientApp');
+
+clientApp.controller('loginControl', function($scope, $window, $http, Credentials) 
+{
+    $scope.usernameInput = "";
+    $scope.passwordInput = "";
+    $scope.loginInfo;
+    
+    $scope.login = function()
+    {
+      console.log("login username =" + $scope.usernameInput);
+      if ($scope.usernameInput === "")
+      {
+        $scope.loginInfo = "Username is blank";
+        return;
+      }
+
+      if ($scope.passwordInput === "")
+      {
+        $scope.loginInfo = "Password is blank";
+        return;
+      }
+
+      var url = "api/users/login";
+      console.log(url);
+      var data =  {
+                  "username" : $scope.usernameInput,
+                  "password" : $scope.passwordInput
+                };
+
+      $http.post(url, data).success(function(data)
+      {
+        console.log(data);
+        if(data.length === 0)
+        {
+          $scope.loginInfo = "Server Error";
+        }
+        else if(data === "Invalid Username")
+          $scope.loginInfo = data;
+        else if(data === "true")
+        {
+          Credentials.setUsername($scope.usernameInput);
+          Credentials.setPassword($scope.passwordInput);
+          $window.location.href = "index.html";
+        }
+        else if(data === "false")
+          $scope.loginInfo = "Invalid Password";
+        else
+          $scope.loginInfo = "Unknown Error";
+      });
+    };
+
+    $scope.createUser = function()
+    {
+      if ($scope.usernameInput === "")
+      {
+        $scope.loginInfo = "Username is blank";
+        return;
+      }
+      if ($scope.passwordInput === "")
+      {
+        $scope.loginInfo = "Password is blank";
+        return;
+      }
+
+      var url = "api/users/register";
+      var data = {
+          "username" : $scope.usernameInput,
+          "password" : $scope.passwordInput
+      };
+      $http.post(url, data).success(function(data)
+      {
+          if(data === "OK")
+          {
+              $scope.loginInfo = "User created";
+          }
+          else
+              $scope.loginInfo = data;
+      });
+    } 
+});
