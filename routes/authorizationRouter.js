ab0bf16e11c5461412444b9f39a05ea2e9f7ce23
Joshua Van Steeter <spam.magnet1991@gmail.com>
2016-06-03 11:43:04 -0600

    Added stuff

--- routes/authorizationRouter.js
+++ routes/authorizationRouter.js
@@ -55,7 +55,7 @@ router.post('/register', function (req, res)
 router.post('/login', passport.authenticate('local'), function (req, res)
 {
 	console.log("Here: " + req);
-	res.sendStatus(200);
+	res.redirect('/index.html');
 });
 
 module.exports = router;
