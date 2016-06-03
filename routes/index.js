11be7942a66851d1da2397a490482e74c065e23b
Joshua Van Steeter <spam.magnet1991@gmail.com>
2016-06-03 12:00:33 -0600

    Added stuff

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,18 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res) 
 {
-  res.redirect('/login.html');
+  	res.redirect('/login');
+});
+
+router.get('/login', function(req, res) 
+{
+	var body = require('../public/login');
+  	res.writeHead(200, 
+  	{
+  		'Content-Length': body.length,
+  		'Content-Type': 'text/html' 
+	});
+	res.send(body);
 });
 
 router.get('/bad', function(req, res)
