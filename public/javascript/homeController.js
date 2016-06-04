2f0491e809acd14b02ff6c92cc8fbb8c3f9f0251
Joshua Van Steeter <spam.magnet1991@gmail.com>
2016-06-03 23:55:43 -0600

    Added Stuff

--- public/javascript/homeController.js
+++ public/javascript/homeController.js
@@ -2,22 +2,22 @@ var clientApp = angular.module('clientApp');
 
 clientApp.controller('homeController', function($scope, $window, $http) 
 {
-  posts = [];
+  	posts = [];
 
-  var post1 = { // TODO get this info from database
-    title: "Welcome to the CS201R Blog",
-    body: "Please feel free to check our new blog website.  Everything should be pretty straight forward.  You are currently on the homepage which displays all posts that have been posted to our server.  Unfortunately they currently are listed from oldest to newest, so any new posts you make will be shown at the bottom.",
-    date: "31 October 2015",
-    author: "System",
-    tags: [
-      "test",
-      "stuff",
-      "blogs",
-      "cats"
-    ]
+  	var post1 = { // TODO get this info from database
+	    title: "Welcome to the CS201R Blog",
+	    body: "Please feel free to check our new blog website.  Everything should be pretty straight forward.  You are currently on the homepage which displays all posts that have been posted to our server.  Unfortunately they currently are listed from oldest to newest, so any new posts you make will be shown at the bottom.",
+	    date: "31 October 2015",
+	    author: "System",
+	    tags: [
+	      "test",
+	      "stuff",
+	      "blogs",
+	      "cats"
+	    ]
   };
   // var url = "getAllPosts?u=" + Credentials.getUsername() + "&p=" + Credentials.getPassword();
-  console.log(url);
+  // console.log(url);
   /*$http.get(url).success(function(data)
   {
     console.log(data);
