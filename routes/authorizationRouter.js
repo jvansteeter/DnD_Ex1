818b7054f00784949902d73a2cf0e19b136e112b
Joshua Van Steeter <spam.magnet1991@gmail.com>
2016-06-04 00:29:50 -0600

    Added Stuff

--- routes/authorizationRouter.js
+++ routes/authorizationRouter.js
@@ -52,8 +52,7 @@ router.post('/register', function (req, res)
 });
 
 router.post('/login',
-  passport.authenticate('local', { successRedirect: '/',
-                                   failureRedirect: '/login' }));
+  passport.authenticate('local'));
 
 // login a local user using passport
 /*router.post('/login', passport.authenticate('local'), function (req, res)
