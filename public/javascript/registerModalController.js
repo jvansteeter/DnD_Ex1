99052b803e14c6c910a3f85a179d5f4918022420
Joshua Van Steeter <spam.magnet1991@gmail.com>
2016-06-07 09:36:27 -0600

    Added Stuff

--- public/javascript/registerModalController.js
+++ public/javascript/registerModalController.js
@@ -10,7 +10,7 @@ clientApp.config(function($modalProvider)
 
 clientApp.controller('modalController', function($scope, $modal, $http) 
 {
-  	//$scope.modal = {title: 'Title', content: 'Hello Modal<br />This is a multiline message!'};
+  	$scope.modal = {title: 'Register New User'};//, content: 'Hello Modal<br />This is a multiline message!'};
 
   	// Controller usage example
   	//
