4b94d72a6d4e75f8399afdff697ec948d82c33d2
Joshua Van Steeter <spam.magnet1991@gmail.com>
2016-06-07 08:46:17 -0600

    Added Stuff

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,11 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res) 
 {
-  	if (req.session.user == null)
+  	if (req.session.user == null && req.path !== "/login.html")
     {
 // if user is not logged-in redirect back to login page //
         console.log('---!!! User not logged in !!!---');
+        console.log('Path: ' + req.path);
         res.sendStatus(401);
     }
     else
