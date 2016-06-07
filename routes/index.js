1b95411214727e11ce9f562723bf39ff5121ebd8
Joshua Van Steeter <spam.magnet1991@gmail.com>
2016-06-07 08:50:49 -0600

    Added Stuff

--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,19 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res) 
 {
-  	if (req.session.user == null && req.path !== "/login.html")
+  	if (req.path === '/' || req.path === '/login.html')
+  	{
+  		res.redirect('/login.html');
+  	}
+  	else if (req.session.user == null)
     {
-// if user is not logged-in redirect back to login page //
         console.log('---!!! User not logged in !!!---');
         console.log('Path: ' + req.path);
         res.sendStatus(401);
     }
     else
     {
-    	res.redirect('/login.html');
+    	res.redirect('/home.html');
     }   
 });
 
