9dc233240f7b704ac4e09c670a840fdb86938209
Joshua Van Steeter <spam.magnet1991@gmail.com>
2016-06-07 15:35:25 -0600

    Added Stuff

--- routes/authorizationRouter.js
+++ routes/authorizationRouter.js
@@ -24,26 +24,23 @@ router.post('/register', function(req, res) //passport.authenticate('local-regis
 	}
 
 	// find or create the user with the given username
-    /*User.findOrCreate(
-    	{
-    		username: username,
-
-    	}, function(err, user, created) 
+    User.findOrCreate({username: req.body.username}, function(err, user, created) 
     {
         if (created) 
         {
-        	console.log("User was created");
+        	console.log("---!!! User was created !!!---");
             // if this username is not taken, then create a user record
-            user.username = username;
+            user.username = req.body.username;
+            user.first_name = req.body.firstname;
+            user.last_name = req.body.lastname;
             user.setPassword(password);
             user.save(function(err) 
             {
 				if (err) 
 				{
-					console.log("There was an error while saving the user");
+					console.log("\tThere was an error while saving the user");
 				    return done(err);
 				}
-		        
 		        return done(null, user);
 		    });
 		} 
@@ -51,9 +48,9 @@ router.post('/register', function(req, res) //passport.authenticate('local-regis
 		{
 		    // return an error if the username is taken
 		    console.log("Username already exists");
-		    return done(null, false, { message: 'User not created' });
+		    return done(null, false, { message: 'Username is already in use' });
 		}
-    });*/
+    });
 
 	res.send("OK");
 });
