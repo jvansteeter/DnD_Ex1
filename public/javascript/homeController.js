0497f197e085e21ad150059e4ff8ae3b451afc18
Joshua Van Steeter <spam.magnet1991@gmail.com>
2016-06-08 16:24:59 -0600

    Added Stuff June 8

--- public/javascript/homeController.js
+++ public/javascript/homeController.js
@@ -9,21 +9,7 @@ clientApp.controller('homeController', function($scope, $window, $http, socket)
 	socket.on('init', function (data) 
   	{
   		console.log(data);
-    	var post1 = 
-    	{ // TODO get this info from database
-		    title: "Welcome to the CS201R Blog",
-		    body: "Please feel free to check our new blog website.  Everything should be pretty straight forward.  You are currently on the homepage which displays all posts that have been posted to our server.  Unfortunately they currently are listed from oldest to newest, so any new posts you make will be shown at the bottom.",
-		    date: "31 October 2015",
-		    author: "System",
-		    tags: [
-		      "test",
-		      "stuff",
-		      "blogs",
-		      "cats"
-		    ]
-		};
-		// posts.push(post1);
-		// $scope.posts = posts;
+
 		var url = "api/encounter/all";
 		var data =  
 		{
@@ -40,8 +26,6 @@ clientApp.controller('homeController', function($scope, $window, $http, socket)
 
   	socket.on('new:encounter', function()
   	{
-  		console.log("Received order to update list of encounters");
-
   		var url = "api/encounter/all";
 		var data =  
 		{
@@ -55,12 +39,4 @@ clientApp.controller('homeController', function($scope, $window, $http, socket)
 			$scope.encounters = data.encounters.reverse();
 		});
   	});
-
-	/*var url = "api/user";
-	console.log(url);
-	$http.get(url).success(function(data)
-	{
-		console.log(data);
-	});*/
-	//$scope.posts.reverse();
 });
--- routes/socket.js
+++ routes/socket.js
@@ -1,76 +1,14 @@
-// Keep track of which names are used so that there are no duplicates
-/*var userNames = (function () 
-{
-    var names = {};
-
-  	var claim = function (name) 
-  	{
-    	if (!name || names[name]) 
-    	{
-      		return false;
-    	} 
-    	else 
-    	{
-      	names[name] = true;
-      	return true;
-    	}
-  	};
-
-  	// find the lowest unused "guest" name and claim it
-  	var getGuestName = function () 
-  	{
-    	var name,
-      	nextUserId = 1;
-
-    	do 
-    	{
-      		name = 'Guest ' + nextUserId;
-      		nextUserId += 1;
-    	} while (!claim(name));
-
-    	return name;
-  	};
-
-  	// serialize claimed names as an array
-  	var get = function () 
-  	{
-    	var res = [];
-    	for (user in names) 
-    	{
-     	 	res.push(user);
-    	}
-
-    	return res;
-  	};
-
-  	var free = function (name) 
-  	{
-    	if (names[name]) 
-    	{
-      		delete names[name];
-    	}
-  	};
-
-  	return {
-    	claim: claim,
-    	free: free,
-    	get: get,
-    	getGuestName: getGuestName
-  	};
-}());*/
-
 var mongoose = require('mongoose');
 var Encounter = mongoose.model('Encounter');
 
 // export function for listening to the socket
 module.exports = function (socket) 
 {
-  	//var name = userNames.getGuestName();
   	console.log('---!!! Connection has been intialized !!!---');
   	// send the new user their name and a list of users
   	socket.emit('init', 
   	{
-    	test: 'test'
+    	connection: 'success'
   	});
 
     socket.on('new:encounter', function(data)
