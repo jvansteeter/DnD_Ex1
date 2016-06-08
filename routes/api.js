f032f9071f32d61ed6a71a523bb5edfd2e7c94b0
Joshua Van Steeter <spam.magnet1991@gmail.com>
2016-06-08 15:44:53 -0600

    Added Stuff June 8

--- routes/api.js
+++ routes/api.js
@@ -38,7 +38,7 @@ router.post('/encounter/create', isLoggedIn, function(req, res)
 router.get('/encounter/all', isLoggedIn, function(req, res)
 {
     console.log("---!!! api/encounter/all call !!!---");
-    Encounter.find({ active : true }, function(error, encounters)
+    Encounter.find({ active : true }).sort([['createdAt', 'descending']]).all(function(error, encounters)
     {
         if (error)
         {
