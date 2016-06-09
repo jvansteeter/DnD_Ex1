0f156dc58768556485733b7b2c5ff9b283fd1296
Joshua Van Steeter <spam.magnet1991@gmail.com>
2016-06-09 09:14:37 -0600

    Added Stuff June 9

--- public/javascript/encounterController.js
+++ public/javascript/encounterController.js
@@ -4,44 +4,11 @@ var clientApp = angular.module('clientApp');
 
 clientApp.controller('encounterController', function($scope, $location, $http, socket) 
 {
-  	console.log("Encounter: " + $location.path());
+    var path = $location.path();
+  	console.log("Encounter: " + path);
 
 	socket.on('init', function (data) 
   	{
   		console.log(data);
-
-		var url = "api/encounter/all";
-		var data =  
-		{
-          	"username" : $scope.usernameInput,
-          	"password" : $scope.passwordInput
-        };
-
-		$http.get(url, data).success(function(data)
-		{
-			console.log(data);
-			$scope.encounters = data.encounters.reverse();
-		});
   	});
-
-  	socket.on('new:encounter', function()
-  	{
-  		var url = "api/encounter/all";
-		var data =  
-		{
-          	"username" : $scope.usernameInput,
-          	"password" : $scope.passwordInput
-        };
-
-		$http.get(url, data).success(function(data)
-		{
-			console.log(data);
-			$scope.encounters = data.encounters.reverse();
-		});
-  	});
-
-  	$scope.join = function ()
-  	{
-  		console.log("Encounter: " + $scope.encounter.title);
-  	}
 });
