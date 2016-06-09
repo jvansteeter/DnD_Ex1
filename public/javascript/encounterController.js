5a11299f13e3c71dcd8eb0d2022e36e4509154ed
Joshua Van Steeter <spam.magnet1991@gmail.com>
2016-06-09 12:11:26 -0600

    Added Stuff June 9

--- public/javascript/encounterController.js
+++ public/javascript/encounterController.js
@@ -2,11 +2,6 @@
 
 var clientApp = angular.module('clientApp');
 
-clientApp.config(function($locationProvider)
-{
-    $locationProvider.html5Mode(true);
-});
-
 clientApp.controller('encounterController', ['$scope', '$location', '$http', 'socket', function($scope, $location, $http, socket) 
 {
     var path = window.location;
