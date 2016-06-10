8f699c464534b809a5d35243fc20ce687385a173
Joshua Van Steeter <spam.magnet1991@gmail.com>
2016-06-10 17:28:37 -0600

    Added Stuff

--- public/javascript/control/encounterAddCharacterController.js
+++ public/javascript/control/encounterAddCharacterController.js
@@ -10,47 +10,46 @@ clientApp.controller('modalController', ['$scope', '$http', '$rootScope', 'Profi
     	{
     		return;
     	}
-
-    	if ($scope.newCharacter.initiative === '' || $scope.newCharacter.initiative < 0 || $scope.newCharacter.initiative > 50)
+    	else if ($scope.newCharacter.initiative === '' || $scope.newCharacter.initiative < 0 || $scope.newCharacter.initiative > 50)
     	{
     		return;
     	}
-
-    	if ($scope.newCharacter.armorClass === '' || $scope.newCharacter.armorClass < 1 || $scope.newCharacter.armorClass > 50)
+    	else if ($scope.newCharacter.armorClass === '' || $scope.newCharacter.armorClass < 1 || $scope.newCharacter.armorClass > 50)
     	{
     		return;
     	}
-
-    	if ($scope.newCharacter.hitPoints === '' || $scope.newCharacter.hitPoints < 0 || $scope.newCharacter.hitPoints > 1000)
+    	else if ($scope.newCharacter.hitPoints === '' || $scope.newCharacter.hitPoints < 0 || $scope.newCharacter.hitPoints > 1000)
     	{
     		return;
     	}
+    	else
+    	{
+	        var socket = io.connect();
+	        var encounterID = Profile.getEncounter();
 
-        var socket = io.connect();
-        var encounterID = Profile.getEncounter();
+	        var url = 'api/encounter/addplayer/' + encounterID;
+	        var data =
+	        {
+	            name : $scope.newCharacter.name,
+	            initiative : $scope.newCharacter.initiative,
+	            armorClass : $scope.newCharacter.armorClass,
+	            maxHitPoints : $scope.newCharacter.hitPoints,
+	            hitPoints : $scope.newCharacter.hitPoints
+	        };
 
-        var url = 'api/encounter/addplayer/' + encounterID;
-        var data =
-        {
-            name : $scope.newCharacter.name,
-            initiative : $scope.newCharacter.initiative,
-            armorClass : $scope.newCharacter.armorClass,
-            maxHitPoints : $scope.newCharacter.hitPoints,
-            hitPoints : $scope.newCharacter.hitPoints
-        };
-
-        $http.post(url, data).success(function(data)
-        {
-            console.log(data);
-            $scope.newCharacter.name = '';
-            $scope.newCharacter.initiative = '';
-            $scope.newCharacter.armorClass = '';
-            $scope.newCharacter.hitPoints = '';
-            console.log("Player successfully added");
-            socket.emit('new:encounterPlayer', 
-            	{
-            		encounterID : encounterID
-            	}, function (){});
-        });
+	        $http.post(url, data).success(function(data)
+	        {
+	            console.log(data);
+	            $scope.newCharacter.name = '';
+	            $scope.newCharacter.initiative = '';
+	            $scope.newCharacter.armorClass = '';
+	            $scope.newCharacter.hitPoints = '';
+	            console.log("Player successfully added");
+	            socket.emit('new:encounterPlayer', 
+	            	{
+	            		encounterID : encounterID
+	            	}, function (){});
+	        });
+    	}
     };
 }]);
