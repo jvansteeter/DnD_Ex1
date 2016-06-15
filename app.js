7df33b0d5cdd2ab677a023d17ccc2b450642f6ad
Joshua Van Steeter <spam.magnet1991@gmail.com>
2016-06-15 09:39:13 -0600

    Added Stuff

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ var authRouter = require("./routes/authorizationRouter");
 var api = require('./routes/api');
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, './public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
