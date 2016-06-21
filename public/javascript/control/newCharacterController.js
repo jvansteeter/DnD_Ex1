da2adfef200c8186eedce22c23e25a6ebdc033ba
Joshua Van Steeter <spam.magnet1991@gmail.com>
2016-06-21 09:14:48 -0600

    Added Stuff

--- public/javascript/control/newCharacterController.js
+++ public/javascript/control/newCharacterController.js
@@ -6,14 +6,51 @@ clientApp.controller('newCharacterController', function($scope, $window, $http, 
 {
     $scope.features = [];
     $scope.proficiencies = [];
+    $scope.languages = [];
+    $scope.attacks = [];
+    $scope.dndLanguages = 
+        [
+            'Dwarvish', 'Elvish', 'Giant', 'Gnomish', 'Goblin', 'Halfling', 'Orc',
+            'Abyssal', 'Celestial', 'Draconic', 'Deep Speech', 'Infernal', 'Primordial', 'Sylvan', 'Undercommon'
+        ];
 
     $scope.addFeature = function()
     {
         $scope.features.push("");
     }
 
+    $scope.removeFeature = function()
+    {
+        $scope.features.splice(-1, 1);
+    }
+
     $scope.addProficiency = function()
     {
         $scope.proficiencies.push("");
+    }
+
+    $scope.removeProficiency = function()
+    {
+        $scope.proficiencies.splice(-1, 1);
+    }
+
+    $scope.addLanguage = function()
+    {
+        $scope.languages.push("");
+    }
+
+    $scope.removeLanguage = function()
+    {
+        $scope.languages.slice(-1, 1);
+    }
+
+    $scope.addAttack = function()
+    {
+        $scope.attacks.push("");
+    }
+    
+    $scope.removeAttack = function()
+    {
+        $scope.attacks.splice(-1, 1);
     }
 });
