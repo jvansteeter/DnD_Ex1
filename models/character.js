6c284742f82d3b3dd48688d4be5a03ca5dbd8779
Joshua Van Steeter <spam.magnet1991@gmail.com>
2016-06-22 09:44:44 -0600

    Added Stuff

--- models/character.js
+++ models/character.js
@@ -15,50 +15,50 @@ var characterSchema = new mongoose.Schema(
     strength: {
     	score: Number,
     	modifier: Number,
-    	save: Boolean,
-    	athletics: Boolean
+    	save: {type: Boolean, required: true, default: false},
+    	athletics: {type: Boolean, required: true, default: false}
     },
     dexterity: {
     	score: Number,
     	modifier: Number,
-    	save: Boolean,
-    	acrobatics: Boolean,
-    	sleight_of_hand: Boolean,
-    	stealth: Boolean
+    	save: {type: Boolean, required: true, default: false},
+    	acrobatics: {type: Boolean, required: true, default: false},
+    	sleight_of_hand: {type: Boolean, required: true, default: false},
+    	stealth: {type: Boolean, required: true, default: false}
     },
     constitution: {
     	score: Number,
     	modifier: Number,
-    	save: Boolean
+    	save: {type: Boolean, required: true, default: false}
     },
     intelligence: {
     	score: Number,
     	modifier: Number,
-    	save: Boolean,
-    	arcana: Boolean,
-    	history: Boolean,
-    	investigation: Boolean,
-    	nature: Boolean,
-    	religion: Boolean
+    	save: {type: Boolean, required: true, default: false},
+    	arcana: {type: Boolean, required: true, default: false},
+    	history: {type: Boolean, required: true, default: false},
+    	investigation: {type: Boolean, required: true, default: false},
+    	nature: {type: Boolean, required: true, default: false},
+    	religion: {type: Boolean, required: true, default: false}
     },
     wisdom: {
     	score: Number,
     	modifier: Number,
-    	save: Boolean,
-    	animal_handling: Boolean,
-    	insight: Boolean,
-    	medicine: Boolean,
-    	perception: Boolean,
-    	survival: Boolean
+    	save: {type: Boolean, required: true, default: false},
+    	animal_handling: {type: Boolean, required: true, default: false},
+    	insight: {type: Boolean, required: true, default: false},
+    	medicine: {type: Boolean, required: true, default: false},
+    	perception: {type: Boolean, required: true, default: false},
+    	survival: {type: Boolean, required: true, default: false}
     },
     charisma: {
     	score: Number,
     	modifier: Number,
-    	save: Boolean,
-    	intimidate: Boolean,
-    	persuasion: Boolean,
-    	performance: Boolean,
-    	deception: Boolean
+    	save: {type: Boolean, required: true, default: false},
+    	intimidate: {type: Boolean, required: true, default: false},
+    	persuasion: {type: Boolean, required: true, default: false},
+    	performance: {type: Boolean, required: true, default: false},
+    	deception: {type: Boolean, required: true, default: false}
     },
     passive_perception: Number,
     armor_class: Number,
@@ -73,7 +73,12 @@ var characterSchema = new mongoose.Schema(
 	ideals: String,
 	bonds: String,
 	flaws: String,
-	attacks: [],
+	attacks: [{
+		name: String,
+		bonus: Number,
+		damage: String,
+		type: String
+	}],
 	money: {
 		copper: Number,
 		silver: Number,
@@ -86,7 +91,7 @@ var characterSchema = new mongoose.Schema(
 		quantity: Number
 	}],
     status: [],
-    npc: Boolean
+    npc: {type: Boolean, required: true, default: false}
 });
 
 mongoose.model('Character', characterSchema);
