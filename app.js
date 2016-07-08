e3a26f6c6f38f3faca212f6ac078b4f9e2671be9
Joshua Van Steeter <spam.magnet1991@gmail.com>
2016-07-08 13:50:33 -0600

    Added Stuff

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Redirect if not logged in
 app.use(function(req, res, next)
