716372ceefd0bff72a79a28f0a1454375ceb0123
Clayton Condie <clayton.condie@gmail.com>
2016-07-27 20:50:41 -0600

    Clayton commit

--- public/javascript/map/init.js
+++ public/javascript/map/init.js
@@ -5,5 +5,5 @@ console.log("this is a script");
 
 var $mapTag = $('#mapDiv');
 // build the mapCanvas
-$mapCanvas = $('<canvas style="border:1px solid #c3c3c3;" id="mapCanvas" width=300"' + '" height=300"' + '" data-index=0"');
+$mapCanvas = $('<canvas style="border:1px solid #c3c3c3;" id="mapCanvas" width="300" height="300" data-index="0"/>');
 $mapTag.append($mapCanvas);
