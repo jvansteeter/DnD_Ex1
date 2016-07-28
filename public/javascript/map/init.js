bf6ed872d621ba8f478e02da5ca8456c19635141
Clayton Condie <clayton.condie@gmail.com>
2016-07-28 12:19:35 -0600

    Clayton commit

--- public/javascript/map/init.js
+++ public/javascript/map/init.js
@@ -1,12 +1,10 @@
 var clientApp = angular.module('clientApp');
 
 clientApp.service('mapInit', function () {
-
     var mapInit = {};
 
     // config variables
     var $mapTag = $('#mapDiv');
-    var count = 0;
 
     // ******************************
     //      Start up Process
@@ -15,13 +13,10 @@ clientApp.service('mapInit', function () {
     // build the mapCanvas
     $mapCanvas = $('<canvas style="border:1px solid #c3c3c3;" id="mapCanvas" width="300" height="300" data-index="0"/>');
     $mapTag.append($mapCanvas);
-    run();
 
-    var run = function () {
-        count++;
-        console.log(count);
-        run();
-    };
+
+
+
 
     return mapInit;
 });
