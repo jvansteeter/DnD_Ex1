61a86f55c5890fcd02655e8a200fc58907c3219e
Joshua Van Steeter <spam.magnet1991@gmail.com>
2016-07-27 22:04:33 -0600

    Added Stuff

--- public/javascript/control/headerController.js
+++ public/javascript/control/headerController.js
@@ -3,6 +3,4 @@ var clientApp = angular.module('clientApp');
 clientApp.controller('headerController', function($scope, Profile)
 {
 	$scope.name = Profile.getFirstName();
-
-	$scope.jtest = "hello world";
 });
