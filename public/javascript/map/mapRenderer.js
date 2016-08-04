1d7040fa2583e794b1c338bef7a019b7b90b8bde
Clayton Condie <clayton.condie@gmail.com>
2016-08-03 18:59:52 -0600

    Changes

---
+++ public/javascript/map/mapRenderer.js
@@ -0,0 +1,23 @@
+var clientApp = angular.module('clientApp');
+
+clientApp.service('mapRenderer', function () {
+
+    var mapRenderer = {};
+
+    mapRenderer.init = function(options){
+        this.$el = options.$el;
+
+        this.w = this.$el.width();
+        this.h = this.$el.height();
+
+        // this.tileSize = options.tileSize;
+        this.context = this.$el.get(0).getContext('2d');
+
+        // this.gameModel = options.gameModel;
+        this.bgImage = this.gameModel.mapImage.getImage();
+
+        this.context.drawImage(this.bgImage,0,0);
+    };
+
+    return mapRenderer;
+});
