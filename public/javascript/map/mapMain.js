b06ae1c857e2b92509818bde0fdf03d4d272c994
Clayton Condie <ccondie@proofpoint.com>
2016-08-04 13:31:05 -0600

    InputController integration

--- public/javascript/map/mapMain.js
+++ public/javascript/map/mapMain.js
@@ -1,6 +1,6 @@
 var clientApp = angular.module('clientApp');
 
-clientApp.service('mapMain', function (mapRenderer) {
+clientApp.service('mapMain', function (mapRenderer, gridRenderer, inputController) {
     var mapMain = {};
     var players;
 
@@ -11,12 +11,19 @@ clientApp.service('mapMain', function (mapRenderer) {
     mapMain.start = function(){
 
         // build the mapCanvas
-        var $mapCanvas = $('<canvas style="border:1px solid #c3c3c3;" id="mapCanvas" width="650" height="650" data-index="0"/>');
+        var $mapCanvas = $('<canvas style="border:1px solid #c3c3c3; z-index:0; position:absolute;" id="mapCanvas" width="650" height="650"/>');
+        mapRenderer.init($mapCanvas);
         $mapTag.append($mapCanvas);
 
-        mapRenderer.init({
-            $el:$mapCanvas
-        });
+        // build the gridCanvas
+        var $gridCanvas = $('<canvas style="border:1px solid #c3c3c3; z-index:1; position:absolute;" id="gridCanvas" width="650" height="650"/>');
+        gridRenderer.init($gridCanvas);
+        $mapTag.append($gridCanvas);
+
+        // build the inputController
+        var $inputCanvas = $('<canvas style="border:1px solid #c3c3c3; z-index:1; position:absolute;" id="inputCanvas" width="650" height="650"/>');
+        inputController.init($inputCanvas);
+        $mapTag.append(inputController);
 
         // run game
         function gameLoop() {
