50e2be850a55b57770e7d6f84512a03a8692ecea
Clayton Condie <clayton.condie@gmail.com>
2016-08-03 19:01:20 -0600

    Changes

--- public/javascript/map/init.js
+++ public/javascript/map/init.js
@@ -1,6 +1,6 @@
 var clientApp = angular.module('clientApp');
 
-clientApp.service('mapInit', function () {
+clientApp.service('mapInit', function (mapRenderer) {
     var mapInit = {};
     var players;
 
