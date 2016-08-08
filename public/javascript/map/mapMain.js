3648c916c217d25dbfac9573cd3d8ba2f40a8c35
Clayton Condie <ccondie@proofpoint.com>
2016-08-08 14:20:42 -0600

    Basic inputController integration. Canvas creation moved into encounter html.

--- public/javascript/map/mapMain.js
+++ public/javascript/map/mapMain.js
@@ -1,6 +1,6 @@
 var clientApp = angular.module('clientApp');
 
-clientApp.service('mapMain', function (mapRenderer, gridRenderer, inputController) {
+clientApp.service('mapMain', function (mapRenderer, gridRenderer) {
     var mapMain = {};
     var gameState;
 
@@ -11,19 +11,14 @@ clientApp.service('mapMain', function (mapRenderer, gridRenderer, inputControlle
     mapMain.start = function(){
 
         // build the mapCanvas
-        var $mapCanvas = $('<canvas style="border:1px solid #c3c3c3; z-index:0; position:absolute;" id="mapCanvas" width="650" height="650"/>');
+        var $mapCanvas = $('#mapCanvas');
         mapRenderer.init($mapCanvas);
-        $mapTag.append($mapCanvas);
 
         // build the gridCanvas
-        var $gridCanvas = $('<canvas style="border:1px solid #c3c3c3; z-index:1; position:absolute;" id="gridCanvas" width="650" height="650"/>');
+        var $gridCanvas = $('#gridCanvas');
         gridRenderer.init($gridCanvas);
-        $mapTag.append($gridCanvas);
 
-        // build the inputController
-        var $inputCanvas = $('<canvas style="border:1px solid #c3c3c3; z-index:1; position:absolute;" id="inputCanvas" width="650" height="650"/>');
-        inputController.init($inputCanvas);
-        $mapTag.append(inputController);
+
 
         // run game
         function gameLoop() {
