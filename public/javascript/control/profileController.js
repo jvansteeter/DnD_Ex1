a5a903eae7b4bee29cd1d73269a5a9e3c76232fe
Joshua Van Steeter <spam.magnet1991@gmail.com>
2016-08-08 21:48:13 -0600

    Finished Profile photo upload, cleaned code.

--- public/javascript/control/profileController.js
+++ public/javascript/control/profileController.js
@@ -4,62 +4,12 @@ var clientApp = angular.module('clientApp');
 
 clientApp.controller('profileController', function($scope, $window, $http, socket, Profile) 
 {
-	var flow;
 	$scope.name = Profile.getFirstName() + " " + Profile.getLastName();
-	$scope.files = [];
 
 	socket.on('init', function (data) 
 	{
 		console.log(data);
 	});
-
-	$scope.initFlow = function($flow)
-	{
-		flow = $flow;
-		var defaultImage = new Image();
-		defaultImage.src = "http://www.placehold.it/200x150/EFEFEF/AAAAAA&text=no+image";
-		console.log("Initializing flow");
-		// var reader = new FileReader();
-		// reader.readAsArrayBuffer(defaultImage);
-		// $flow.files[0] = reader.result;
-		// $flow.files[0] = defaultImage;
-	};
-
-	$scope.seeFlow = function()
-	{
-		console.log(flow);
-	};
-
-	$scope.saveNoImage = function()
-	{
-		var url = 'api/image/no';
-		var data = flow.files[0];
-		var fd = new FormData();
-
-		fd.append("file", flow.files[0]);
-
-		$http.post(url, fd, {
-			headers:
-			{
-				'Content-Type': undefined
-			},
-            transformRequest : angular.identity
-		}).success(function(data)
-		{
-			console.log(data);
-		});
-	};
-
-	$scope.getNoImage = function()
-    {
-    	$('#myImage').html('<img src="api/image/no">');
-        // var url = 'api/image/no';
-        //
-        // $http.get(url).success(function(data)
-        // {
-        //     $scope.image = data.Image;
-        // });
-    };
 
 	$scope.uploadProfilePhoto = function($flow)
 	{
@@ -86,35 +36,6 @@ clientApp.controller('profileController', function($scope, $window, $http, socke
 				console.log(data);
 			});
 	};
-
-	// $scope.uploadImage = function(event)
-	// {
-	// 	console.log("Attempting to upload image");
-	// 	console.log(event);
-	// 	var files = $scope.files;
-    //
-	// 	var url = 'api/image/profile';
-	// 	for ( var i = 0; i < files.length; i++)
-	// 	{
-	// 		var fd = new FormData();
-	// 		fd.append("file", files[i]);
-	// 		$http.post(url, fd, {
-	// 			withCredentials : false,
-	// 			headers : {
-	// 				'Content-Type' : undefined
-	// 			},
-	// 			transformRequest : angular.identity
-	// 		})
-	// 			.success(function(data)
-	// 			{
-	// 				console.log(data);
-	// 			})
-	// 			.error(function(data)
-	// 			{
-	// 				console.log(data);
-	// 			});
-	// 	}
-	// };
 
 	$scope.listModalgetCharacters = function()
 	{
