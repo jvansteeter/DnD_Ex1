28c37970efa31f28a34fd4011465fd63b5117950
Joshua Van Steeter <spam.magnet1991@gmail.com>
2016-08-12 15:33:16 -0600

    Removed unneccessary stuff from home page

--- public/javascript/control/homeController.js
+++ public/javascript/control/homeController.js
@@ -4,39 +4,8 @@ var clientApp = angular.module('clientApp');
 
 clientApp.controller('homeController', function($scope, $window, $http, socket) 
 {
-	var posts = [];
-
-	socket.on('init', function (data) 
+	socket.on('init', function (data)
 	{
 		console.log(data);
-
-		var url = "api/encounter/all";
-		var data =  
-		{
-			"username" : $scope.usernameInput,
-			"password" : $scope.passwordInput
-		};
-
-		$http.get(url, data).success(function(data)
-		{
-			console.log(data);
-			$scope.encounters = data.encounters.reverse();
-		});
-	});
-
-	socket.on('new:encounter', function()
-	{
-		var url = "api/encounter/all";
-		var data =  
-		{
-			"username" : $scope.usernameInput,
-			"password" : $scope.passwordInput
-		};
-
-		$http.get(url, data).success(function(data)
-		{
-			console.log(data);
-			$scope.encounters = data.encounters.reverse();
-		});
 	});
 });
