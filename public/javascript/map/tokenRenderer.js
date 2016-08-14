be2d77961d728b7dde79da44a26a755fc8d44842
Clayton Condie <clayton.condie@gmail.com>
2016-08-13 22:43:03 -0600

    Adds host visibilty rules to tokenRenderer

--- public/javascript/map/tokenRenderer.js
+++ public/javascript/map/tokenRenderer.js
@@ -9,36 +9,69 @@ clientApp.service('tokenRenderer', function () {
     var width;
     var height;
     var tileSize;
+    var isHost = false;
 
-    tokenRenderer.init = function(canvasParam, encounterStateParam){
+    tokenRenderer.init = function(canvasParam, isHostParam){
 
         canvas = canvasParam;
 
         width = canvas.width();
         height = canvas.height();
         context = canvas.get(0).getContext('2d');
-        encounterState = encounterStateParam;
         tileSize = 50;
+        isHost = isHostParam;
 
+    };
+
+    tokenRenderer.updateEncounterState = function(newEncounterState){
+        encounterState = newEncounterState;
     };
 
     tokenRenderer.draw = function(){
         context.clearRect(0, 0, width, height);
 
+        //for each player entry in the encounter JSON
         for(var i = 0; i < encounterState.players.length; i++){
+            //collect the current player
+            var player = encounterState.players[i];
+
+            //identify the icon for the player
             var tokenImage = new Image();
-            if(!encounterState.players[i].npc){
+            if(!player.npc){
                 tokenImage.src = "image/map/playerOne.png";
             }
             else {
                 tokenImage.src = "image/map/playerThree.png";
             }
 
-            context.drawImage(
-                tokenImage,
-                i * tileSize,
-                1 * tileSize
-            );
+            //draw the icon based off of current settings
+            if(player.visible){
+                context.globalAlpha = 1;
+                context.drawImage(
+                    tokenImage,
+                    i * tileSize,
+                    1 * tileSize
+                );
+            }
+            else{
+                if(isHost){
+                    context.globalAlpha = 0.35;
+                    context.drawImage(
+                        tokenImage,
+                        i * tileSize,
+                        1 * tileSize
+                    );
+                }
+                else{
+                    context.globalAlpha = 0.0;
+                    context.drawImage(
+                        tokenImage,
+                        i * tileSize,
+                        1 * tileSize
+                    );
+                }
+
+            }
         }
     };
 
