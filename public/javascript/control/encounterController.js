3a7270bdada8607a7e48cd9d5cee3f34df2210e9
Clayton Condie <clayton.condie@gmail.com>
2016-08-15 12:16:18 -0600

    Temp remove map elements

--- public/javascript/control/encounterController.js
+++ public/javascript/control/encounterController.js
@@ -15,7 +15,7 @@ clientApp.controller('encounterController', function($scope, $http, $q, socket, 
 			$scope.host = Encounter.isHost();
 			$scope.encounter = Encounter.getEncounterState();
 			console.log($scope.encounter);
-			mapMain.setGameState(Encounter.isHost());
+			// mapMain.setGameState(Encounter.isHost());
 		});
 		// Profile.async().then(function()
 		// {
