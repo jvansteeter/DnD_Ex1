9b0ec7556cead73af98e9e70e353e706086e717b
Clayton Condie <clayton.condie@gmail.com>
2016-08-15 22:14:15 -0600

    Refactor of gridRenderer to work as controller

--- public/javascript/map/gridRenderer.js
+++ public/javascript/map/gridRenderer.js
@@ -1,36 +1,35 @@
 var clientApp = angular.module('clientApp');
 
-clientApp.service('gridRenderer', function () {
+clientApp.controller('gridRenderer', function ($window) {
 
-    var gridRenderer = {};
     var canvas;
     var context;
     var width;
     var height;
     var tileSize;
 
-    gridRenderer.init = function(canvasParam){
-
-        canvas = canvasParam;
-
+    function init() {
+        canvas = $('#gridCanvas');
         width = canvas.width();
         height = canvas.height();
         context = canvas.get(0).getContext('2d');
-        tileSize = 50;
-    };
 
-    gridRenderer.draw = function(){
+        draw();
+    }
+
+    function draw() {
         context.clearRect(0, 0, width, height);
         context.fillStyle = "rgba(255,0,0,.3)";
-        for(i = 0; i < 12; i++){
-            context.fillRect(tileSize * i, 0, 2, 650);
+        for (var vert = 0; vert < 12; vert++) {
+            context.fillRect(50 * vert, 0, 2, 650);
         }
 
-        for(i = 0; i < 12; i++){
-            context.fillRect(0, tileSize * i, 650, 2);
+        for (var hori = 0; hori < 12; hori++) {
+            context.fillRect(0, 50 * hori, 650, 2);
         }
-    };
 
+        $window.requestAnimationFrame(draw);
+    }
 
-    return gridRenderer;
+    init();
 });
