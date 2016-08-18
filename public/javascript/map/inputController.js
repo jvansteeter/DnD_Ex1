0099673fda72b87a9c59a9174552146a576d2ab5
Clayton Condie <clayton.condie@gmail.com>
2016-08-18 10:01:33 -0600

    Basic mousewheel integration

--- public/javascript/map/inputController.js
+++ public/javascript/map/inputController.js
@@ -1,31 +1,30 @@
 var clientApp = angular.module('clientApp');
 
-clientApp.controller('inputController', function($scope, Encounter)
-{
-   var canvas;
-   var context;
-   var width;
-   var height;
+clientApp.controller('inputController', function ($scope, Encounter) {
+    var canvas;
+    var context;
+    var width;
+    var height;
 
-   function init(){
-      canvas = $('#inputCanvas');
-      width = canvas.width();
-      height = canvas.height();
-      context = canvas.get(0).getContext('2d');
-   }
+    function init() {
+        canvas = $('#inputCanvas');
+        width = canvas.width();
+        height = canvas.height();
+        context = canvas.get(0).getContext('2d');
+    }
 
-   $scope.click = function(){
-      console.log('click');
-   };
+    $scope.click = function () {
+        console.log('click');
+    };
 
-   $scope.mouseMove = function(){
-     console.log('mouseMove');
-   };
+    $scope.mouseMove = function () {
+        console.log('mouseMove');
+    };
 
-   $scope.mouseScroll = function($event, $delta, $deltaX, $deltaY) {
-      console.log("delta: " + $delta + " deltaX: " + $deltaX + " deltaY: " + $deltaY);
-   };
+    $scope.mouseScroll = function (event, delta, deltaX, deltaY) {
+        console.log("delta: " + delta + " deltaX: " + deltaX + " deltaY: " + deltaY);
+    };
 
 
-   init();
+    init();
 });
