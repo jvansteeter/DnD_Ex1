9d62a5fcca11ea912285ba80b42c98657d029fe7
Clayton Condie <clayton.condie@gmail.com>
2016-08-18 08:19:01 -0600

    begin dev of inputController. adds hamsterjs to bower_components

--- public/javascript/map/inputController.js
+++ public/javascript/map/inputController.js
@@ -1,7 +1,26 @@
 var clientApp = angular.module('clientApp');
 
-clientApp.controller('inputController', function($scope)
+clientApp.controller('inputController', function($scope, Encounter)
 {
-   $scope.testClick = function(){
+   var canvas;
+   var context;
+   var width;
+   var height;
+
+   function init(){
+      canvas = $('#inputCanvas');
+      width = canvas.width();
+      height = canvas.height();
+      context = canvas.get(0).getContext('2d');
+   }
+
+   $scope.click = function(){
+      console.log('click');
    };
+
+   $scope.mouseMove = function(){
+     console.log('mouseMove');
+   };
+
+   init();
 });
