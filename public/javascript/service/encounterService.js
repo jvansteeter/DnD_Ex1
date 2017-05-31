898cab7abbd1d55767085f112d3b669e60343ce7
Clayton Condie <crcondie@verisk.com>
2017-05-31 13:54:18 -0600

    start of notes in model

--- public/javascript/service/encounterService.js
+++ public/javascript/service/encounterService.js
@@ -24,6 +24,7 @@ clientApp.factory('EncounterService', function ($http, $q, Profile, socket)
         [255,102,0],
         [102,0,204]
     ];
+
     encounterService.notes = [
         [{x:0,y:0}, {x:1,y:1}, {x:2,y:2}, {x:3,y:3}, {x:4,y:4}],
         [{x:1,y:0}, {x:2,y:1}, {x:3,y:2}, {x:4,y:3}, {x:0,y:4}],
@@ -143,6 +144,10 @@ clientApp.factory('EncounterService', function ($http, $q, Profile, socket)
     encounterService.setUpdateHasRunFlag = function(value)
     {
         this.updateHasRun = value;
+    };
+
+    encounterService.pushNotes = function(){
+
     };
 
     return encounterService;
